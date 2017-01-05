529b6e5b6a02ae3d539d1bbd4ed7830dedbde118
misoton665 <misoton998@gmail.com>
2017-01-05 12:48:39 +0900

    update

--- src/app.rb
+++ src/app.rb
@@ -1,47 +1,28 @@
 require 'sinatra'
 
-# 名前を保存するリポジトリ
-module NameRepository
-  @@names = {}
-
-  def self.add(session_id, name)
-    @@names[session_id] = name
-  end
-
-  def self.find(session_id)
-    @@names[session_id]
-  end
-end
-
 class MainApp < Sinatra::Base
   configure do
     # セッション管理を有効にする
     enable :sessions
   end
 
-  post '/namepost' do
-    posted_name = request.body.read
-
-    # セッションIDを生成して名前と一緒に保存
-    session_id = Digest::SHA256.hexdigest(rand.to_s)
-    session[:id] = session_id
-    NameRepository.add(session_id, posted_name)
-
-    # レスポンス
-    status 201
-    "OK, I've memorized you.\n"
+  get '/start_session' do
+    session[:started] = true
+    "A session has stared.\nYour session ID is \"#{session.id}\""
   end
 
-  get '/namepost' do
-    session_id = session[:id]
-    saved_name = NameRepository.find(session_id)
+  get '/destroy_session' do
+    destroied_session_id = session.id
+    session.destroy
+    "The session (#{destroied_session_id}) has destroied.\n"
+  end
 
-    # 名前が保存されていない場合は別のメッセージ
-    if !saved_name
-    return "Who are you???\n"
+  get '/check_session' do
+    if session[:started] then
+      "You started the session.\nYour session ID is \"#{session.id}\""
+    else
+      "Did you start a session?\n"
     end
-
-    "Oh! My friend #{saved_name}!!\n"
   end
 
 end
